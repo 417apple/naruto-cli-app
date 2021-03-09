cc9f3e4f6e0b476f4d774c1b0d5a386c1a899b38
YuriSuzuki0901 <yuri.suzuki@tam-bourine.co.jp>
2021-03-09 22:16:50 +0900

    現在何ターン目なのかの表示追加

--- main.php
+++ main.php
@@ -8,13 +8,20 @@ $enemy = new Enemy();
 $human->name = "ナルト";
 $enemy->name = "ペイン";
 
+$turn = 1;
+
 while($human->hitPoint > 0 && $enemy->hitPoint >0){
+
+    echo "***" . $turn . "ターン目***\n\n";
+
     echo $human->name . ":" . $human->hitPoint . "/" . $human::MAX_HITPOINT . "\n";
     echo $enemy->name . ":" . $enemy->hitPoint . "/" . $enemy::MAX_HITPOINT . "\n";
     $human->doAttack($enemy);
     $enemy->doAttack($human);
-    echo "\n";
+    echo "\n\n";
+    $turn ++;
 }
+
 echo "戦闘終了\n";
 echo $human->name . ":" . $human->hitPoint . "/" . $human::MAX_HITPOINT . "\n";
 echo $enemy->name . ":" . $enemy->hitPoint . "/" . $enemy::MAX_HITPOINT . "\n";
